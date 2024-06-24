717e3238a80e1a4e77bbbd4a9166222fd295db58
whelew <lewisswheelerr@outlook.com>
2024-06-24 21:22:26 +0100

    added function to populate normal and hard game board

--- assets/script.js
+++ assets/script.js
@@ -10,10 +10,12 @@ function loadBoard(event) {
             populateEasyBoard();
             break;
         case "normal":
-            levelSelect.innerHTML = `<div class="game-board" id="normal-board"><p>game board 2</p></div>`
+            levelSelect.innerHTML = `<div class="game-board" id="normal-board"></div>`
+            populateNormalBoard()
             break; 
         case "hard":
-            levelSelect.innerHTML = `<div class="game-board" id="hard-board"><p>game board 3</p></div>`
+            levelSelect.innerHTML = `<div class="game-board" id="hard-board"></div>`
+            populateHardBoard()
             break;
         case "leader-board":
             alert ("You clicked the leaderboard button");
@@ -24,11 +26,11 @@ function loadBoard(event) {
 
 
 function runGame() {
+    
+} 
 
-}
 
-
-
+//populates game board with tiles
 function populateEasyBoard() {
     var board = [];
     var rows = 8;
@@ -40,6 +42,44 @@ function populateEasyBoard() {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             document.getElementById("easy-board").append(tile)
+            row.push(tile);
+        }
+        board.push(row);
+    }
+    console.log(board)    
+}
+
+//populates game board with tiles
+function populateNormalBoard() {
+    var board = [];
+    var rows = 12;
+    var columns = 12;
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            document.getElementById("normal-board").append(tile)
+            row.push(tile);
+        }
+        board.push(row);
+    }
+    console.log(board)    
+}
+
+//populates game board with tiles
+function populateHardBoard() {
+    var board = [];
+    var rows = 12;
+    var columns = 20;
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            document.getElementById("hard-board").append(tile)
             row.push(tile);
         }
         board.push(row);
