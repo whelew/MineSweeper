316aef968f69c2a48da3dee58e33592623862050
whelew <lewisswheelerr@outlook.com>
2024-06-24 14:38:37 +0100

    created menu buttons and added basic styling in css

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function populateGameBoard() {
+
+}
+
+function incrementFlag() {
+
+}
+
+function revealTile() {
+
+}
+
+function markTile() {
+
+}
+
+function checkTile() {
+
+}
+
+function restartGame() {
+
+}
+
+function createBoard() {
+    //<h1>Mines:<span id="mines-counter"></span></h1>
+    //<div id="board"></div>
+}
