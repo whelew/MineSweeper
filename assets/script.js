23f87fb0ee9c923427b9edcc14b0edd7deb064a5
whelew <lewisswheelerr@outlook.com>
2024-06-25 13:58:19 +0100

    condensed 3 functions into one new function that calls each rows, columns and  boardId as arguments

--- assets/script.js
+++ assets/script.js
@@ -7,15 +7,15 @@ function loadBoard(event) {
     switch (event.target.id) {
         case "easy":
             levelSelect.innerHTML = `<div class="game-board" id="easy-board"></div>`
-            populateEasyBoard();
+            populateGameBoard(8, 8, "easy-board");
             break;
         case "normal":
             levelSelect.innerHTML = `<div class="game-board" id="normal-board"></div>`
-            populateNormalBoard()
+            populateGameBoard(12, 12, "normal-board");
             break; 
         case "hard":
             levelSelect.innerHTML = `<div class="game-board" id="hard-board"></div>`
-            populateHardBoard()
+            populateGameBoard(12, 20, "hard-board");
             break;
         case "leader-board":
             alert ("You clicked the leaderboard button");
@@ -29,63 +29,23 @@ function runGame() {
     
 } 
 
-
-//populates game board with tiles
-function populateEasyBoard() {
+//populates easy game board with tiles
+function populateGameBoard(rows, columns, boardId) {
     var board = [];
-    var rows = 8;
-    var columns = 8;
 
     for (let r = 0; r < rows; r++) {
         let row = [];
         for (let c = 0; c < columns; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
-            document.getElementById("easy-board").append(tile)
+            document.getElementById(boardId).append(tile)
             row.push(tile);
         }
         board.push(row);
     }
-    console.log(board)    
+    console.log(board);    
 }
 
-//populates game board with tiles
-function populateNormalBoard() {
-    var board = [];
-    var rows = 12;
-    var columns = 12;
-
-    for (let r = 0; r < rows; r++) {
-        let row = [];
-        for (let c = 0; c < columns; c++) {
-            let tile = document.createElement("div");
-            tile.id = r.toString() + "-" + c.toString();
-            document.getElementById("normal-board").append(tile)
-            row.push(tile);
-        }
-        board.push(row);
-    }
-    console.log(board)    
-}
-
-//populates game board with tiles
-function populateHardBoard() {
-    var board = [];
-    var rows = 12;
-    var columns = 20;
-
-    for (let r = 0; r < rows; r++) {
-        let row = [];
-        for (let c = 0; c < columns; c++) {
-            let tile = document.createElement("div");
-            tile.id = r.toString() + "-" + c.toString();
-            document.getElementById("hard-board").append(tile)
-            row.push(tile);
-        }
-        board.push(row);
-    }
-    console.log(board)    
-}
 
 function incrementFlag() {
 
