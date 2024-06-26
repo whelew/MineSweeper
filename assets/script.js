06991acf34c4d4dd0e5696abea9442df9c4175ac
whelew <lewisswheelerr@outlook.com>
2024-06-26 20:55:00 +0100

    added timer to innerHTML of levelSelect

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,8 @@ function loadBoard(event) {
     switch (event.target.id) {
         case "easy":
             levelSelect.innerHTML = `
-            <p class="Flag-counter"><img src="assets/images/flag.png">Flags Remaining:<span id="count">10</span></p>
+            <p class="Flag-counter"><img src="assets/images/flag.png">Flags Remaining:<span id="count">10</span>
+            Timer:<span id="timer">0</span></p>
             <div class="game-board" id="easy-board"></div>
             `
             populateGameBoard(8, 8, "easy-board");
@@ -16,7 +17,8 @@ function loadBoard(event) {
             break;
         case "normal":
             levelSelect.innerHTML = `
-            <p class="Flag-counter"><img src="assets/images/flag.png">Flags Remaining:<span id="count">30</span></p>
+            <p class="Flag-counter"><img src="assets/images/flag.png">Flags Remaining:<span id="count">30</span>
+            Timer:<span id="timer">0</span></p>
             <div class="game-board" id="normal-board"></div>`
             populateGameBoard(12, 12, "normal-board");
             setMines(30, 12, 12);
@@ -24,7 +26,8 @@ function loadBoard(event) {
             break; 
         case "hard":
             levelSelect.innerHTML = `
-            <p class="Flag-counter"><img src="assets/images/flag.png">Flags Remaining:<span id="count">50</span></p>
+            <p class="Flag-counter"><img src="assets/images/flag.png">Flags Remaining:<span id="count">50</span>
+            Timer:<span id="timer">0</span></p>
             <div class="game-board" id="hard-board"></div>`
             populateGameBoard(12, 20, "hard-board");
             setMines(50, 12, 20);
