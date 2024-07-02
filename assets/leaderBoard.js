10caea200269c45908b26d3b5ef986b7462ff58a
whelew <lewisswheelerr@outlook.com>
2024-07-02 19:04:25 +0100

    adjusted updateLeaderBoard function, it was not looping through the for loop correctly

--- assets/leaderBoard.js
+++ assets/leaderBoard.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", updateLeaderBoard)
+document.addEventListener("DOMContentLoaded", function() {
+    updateLeaderBoard();
+}); 
 
 let lBoardBtn = document.getElementById("l-menu");
 lBoardBtn.addEventListener("click", loadMainMenu)
@@ -13,20 +15,37 @@ function updateLeaderBoard() {
     let score = parseInt(localStorage.getItem("score-value"));
     
     if (name && !isNaN(score)) {
-        let leaderBoard = [];
-        for (let i = 0; i <= 5; i++) {
-            let leaderName = document.getElementById(`l-name-${i}`).innerText; //stores all names of current lboard
-            let leaderScore = parseInt(document.getElementById(`l-score-${i}`).innerText); //stores all scores of current lboard
-            leaderBoard.push({name: leaderName, score: leaderScore}); //pushes value into array
-        }
+        let leaderBoard = [
+            {name: document.getElementById("l-name-1").innerText,
+                score: parseInt(document.getElementById('l-score-1').innerText )
+            },
+            {name: document.getElementById("l-name-2").innerText,
+                score: parseInt(document.getElementById('l-score-2').innerText )
+            },
+            {name: document.getElementById("l-name-3").innerText,
+                score: parseInt(document.getElementById('l-score-3').innerText )
+            },
+            {name: document.getElementById("l-name-4").innerText,
+                score: parseInt(document.getElementById('l-score-4').innerText )
+            },
+            {name: document.getElementById("l-name-5").innerText,
+                score: parseInt(document.getElementById('l-score-5').innerText )
+            },
+        ];
+        //push new value into array
         leaderBoard.push({name: name, score: score});
-        leaderBoard.sort((a, b) => b.score - a.score);
+        leaderBoard.sort((a, b) => a.score - b.score);
         leaderBoard.slice(0, 5);
 
-        leaderBoard.forEach(entry, index) => {
-            document.getElementById(`l-name-${index + 1}`).innerText = entry.name;
-            document.getElementById(`l-score-${index + 1}`).innerText = entry.score;
-        }
+        leaderBoard.forEach((entry, index) => {
+            let nameEle = document.getElementById(`l-name-${index + 1}`);
+            let scoreEle = document.getElementById(`l-score-${index + 1}`);
+
+            if (nameEle && scoreEle) {
+                nameEle.innerText = entry.name;
+                scoreEle.innerText = entry.score;
+            }
+        })
     }
 }
 
